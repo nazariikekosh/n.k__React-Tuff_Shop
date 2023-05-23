2f024e5f0e3672d5b06bb1ebe9e63c9cd642c9cc
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-05-23 19:04:35 +0300

    add__form

--- tuff-app/src/components/Header/Header.jsx
+++ tuff-app/src/components/Header/Header.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ROUTES } from '../../utils/routes';
-import LOGO from "../../images/logo.svg";
-
-
+import LOGO from '../../images/logo.svg';
 
 import styles from './../../styles/Header.module.css';
+// import AVATAR from '../../images/avatar.jpg ';
 
 const Header = () => {
 	return (
 		<div className={styles.header}>
 			<div className={styles.logo}>
 				<Link to={ROUTES.HOME}>
-                    <img src={LOGO} alt="Stuff" />
-                </Link>
+					<img src={LOGO} alt='Stuff' />
+				</Link>
+			</div>
+			<div className={styles.info}>
+				<div className={styles.user}>
+					<div className={styles.avatar}
+                    //  style={{backgroundImage: `url(${AVATAR})`}} 
+                    />
+                    <div className={styles.username}>Guest</div>
+				</div>
+                <form className={styles.form}>
+                <div className={styles.icon}>
+                    <svg>
+                        <use xlinkHref={`${process.env.PUBLIC_URL}/splite.svg#search`} />
+                    </svg>
+                </div>
+                <div className={styles.input}>
+                    <input type="search" name="search" placeholder="Search for anything..." />
+                </div>
+                </form>
 			</div>
 		</div>
 	);
