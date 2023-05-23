2cd0e2ae35fb4824b226fde443f716c323ec439e
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-05-23 16:49:52 +0300

    add__logo

--- tuff-app/src/components/Header/Header.jsx
+++ tuff-app/src/components/Header/Header.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ROUTES } from '../../utils/routes';
+import LOGO from "../../images/logo.svg";
+
+
+
+import styles from './../../styles/Header.module.css';
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+	return (
+		<div className={styles.header}>
+			<div className={styles.logo}>
+				<Link to={ROUTES.HOME}>
+                    <img src={LOGO} alt="Stuff" />
+                </Link>
+			</div>
+		</div>
+	);
+};
 
-export default Header
+export default Header;
