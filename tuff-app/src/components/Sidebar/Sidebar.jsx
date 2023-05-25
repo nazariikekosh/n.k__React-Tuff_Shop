41c73180fea5d1fb4252595a6a13f2094970df3e
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-05-25 14:58:23 +0300

    add__sidebar

--- tuff-app/src/components/Sidebar/Sidebar.jsx
+++ tuff-app/src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+import styles from "../../styles/Sidebar.module.css";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <section className={styles.sidebar}>
+      <div className={styles.title}>CATEGORIES</div>
+      <nav>
+        <ul className={styles.menu}>
+            <li>
+              <NavLink>
+                  Link
+              </NavLink>
+            </li>
+        </ul>
+      </nav>
+      <div className={styles.footer}>
+        <a href="/help" target='_blank' className={styles.link}>
+          Help
+        </a>
+
+        <a href="/Terms" target='_blank' className={styles.link} style={{textDecoration:"underline"}}>
+        Terms & Conditions
+        </a>
+
+      </div>
+    </section>
   )
 }
 
